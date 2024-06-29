cf542cb08a8ecb624e2b42df5775e0d4c4aa3d63
hyeonseok98 <157561573+hyeonseok98@users.noreply.github.com>
2024-06-30 05:56:25 +0900

    update: api error 처리 추가

--- src/api/country.api.ts
+++ src/api/country.api.ts
@@ -4,7 +4,14 @@ import { Country } from "../types/country.type";
 const BASE_URL = "https://restcountries.com/v3.1/all";
 
 export const countryApi = async (): Promise<Country[]> => {
-  const response = await axios.get<Country[]>(BASE_URL);
-  const data = response.data;
-  return data;
+  try {
+    const response = await axios.get<Country[]>(BASE_URL);
+    const data = response.data;
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("API를 불러오는 도중 에러 발생: ", error);
+    }
+    return [];
+  }
 };
